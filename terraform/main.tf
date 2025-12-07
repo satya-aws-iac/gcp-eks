302d05f23dbc402991feb061957d56f86fa76b09
satya-aws-iac <satyanarayanamylavarapuaws@gmail.com>
2025-12-07 22:06:08 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,21 +70,3 @@ resource "google_container_cluster" "autopilot" {
     }
   }
 }
-
-# Outputs
-output "cluster_name" {
-  description = "GKE Autopilot cluster name"
-  value       = google_container_cluster.autopilot.name
-}
-
-output "cluster_endpoint" {
-  description = "GKE cluster endpoint"
-  value       = google_container_cluster.autopilot.endpoint
-  sensitive   = true
-}
-
-output "cluster_ca_certificate" {
-  description = "GKE cluster CA certificate"
-  value       = google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate
-  sensitive   = true
-}
