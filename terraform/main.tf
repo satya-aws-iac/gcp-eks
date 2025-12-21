2fd0336b6adf66e0dfcfadc833afd5b9a3d5de0d
satya-aws-iac <satyanarayanamylavarapuaws@gmail.com>
2025-12-21 14:13:54 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,49 +17,6 @@ provider "google" {
 }
 
 # VPC Network
-resource "google_compute_network" "vpc" {
-  name                    = "${var.cluster_name}-vpc"
-  auto_create_subnetworks = false
-}
-
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.cluster_name}-subnet"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc.id
-
-  secondary_ip_range {
-    range_name    = "pods"
-    ip_cidr_range = "10.1.0.0/16"
-  }
-
-  secondary_ip_range {
-    range_name    = "services"
-    ip_cidr_range = "10.2.0.0/16"
-  }
-}
-
-# Standard GKE Cluster
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.zone
-
-  deletion_protection      = false
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
-  }
-
-  workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
-  }
 
   master_authorized_networks_config {
     cidr_blocks {
