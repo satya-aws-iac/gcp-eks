7c8bfa07e188b6efce59af25cd81b265d593233d
satya-aws-iac <satyanarayanamylavarapuaws@gmail.com>
2025-12-21 14:28:41 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,50 @@ provider "google" {
   region  = var.region
 }
 
-# VPC Network
+// VPC Network
+resource "google_compute_network" "vpc" {
+  name                    = "${var.cluster_name}-vpc"
+  auto_create_subnetworks = false
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.cluster_name}-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.2.0.0/16"
+  }
+}
+
+# Standard GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
+
+  deletion_protection      = false
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 
   master_authorized_networks_config {
     cidr_blocks {
@@ -51,38 +94,38 @@ provider "google" {
 #   location   = var.zone
 #   cluster    = google_container_cluster.primary.name
 #   node_count = 2
-
+#
 #   node_config {
 #     preemptible  = true
 #     machine_type = "e2-small"
 #     disk_size_gb = 30
 #     disk_type    = "pd-standard"
-
+#
 #     service_account = google_service_account.gke_nodes.email
 #     oauth_scopes = [
 #       "https://www.googleapis.com/auth/cloud-platform"
 #     ]
-
+#
 #     labels = {
 #       env = var.environment
 #     }
-
+#
 #     tags = ["gke-node", "${var.cluster_name}-gke"]
-
+#
 #     metadata = {
 #       disable-legacy-endpoints = "true"
 #     }
-
+#
 #     workload_metadata_config {
 #       mode = "GKE_METADATA"
 #     }
 #   }
-
+#
 #   autoscaling {
 #     min_node_count = 1
 #     max_node_count = 3
 #   }
-
+#
 #   management {
 #     auto_repair  = true
 #     auto_upgrade = true
